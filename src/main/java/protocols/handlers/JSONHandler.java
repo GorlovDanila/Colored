ad27cd3258eef60807403214dd9fdc227284e0eb
pavlushaemae <pavel.pidikseev173@gmail.com>
2022-12-12 15:19:01 +0300

    Added protocol

---
+++ src/main/java/protocols/handlers/JSONHandler.java
@@ -0,0 +1,70 @@
+package protocols.handlers;
+
+import com.google.gson.Gson;
+import protocols.MessagePacket;
+import protocols.PacketField;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+import static protocols.MessagePacket.FOOTER_1;
+import static protocols.MessagePacket.FOOTER_2;
+
+public class JSONHandler {
+    static Gson gson = new Gson();
+
+    public static void toByteArray(MessagePacket packet, ByteArrayOutputStream bos) {
+        bos.write(MessagePacket.SUBTYPE_JSON);
+        try {
+            for (PacketField field: packet.getFields()) {
+                bos.write(new byte[] {field.getFieldId(), field.getFieldSize()});
+                bos.write(field.getContent());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static MessagePacket parse(byte[] data) {
+        MessagePacket packet = MessagePacket.create(MessagePacket.TYPE_BOARD, MessagePacket.SUBTYPE_JSON);
+        int offset = 5;
+        while (true) {
+            if (data.length - 2 <= offset) {
+                return packet;
+            }
+            byte fieldId = data[offset];
+            byte fieldSize = data[offset + 1];
+
+            if (fieldId == FOOTER_1 && fieldSize == FOOTER_2) {
+                return packet;
+            }
+
+            byte[] content = new byte[fieldSize];
+            if (fieldSize != 0) {
+                System.arraycopy(data, offset + 2, content, 0, fieldSize);
+            }
+
+            PacketField field = new PacketField(fieldId, fieldSize, content);
+            packet.getFields().add(field);
+
+            offset += 2 + fieldSize;
+        }
+    }
+
+    public static PacketField setValue(PacketField field, Object value) {
+        try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
+             ObjectOutputStream oos = new ObjectOutputStream(bos)) {
+            oos.writeObject(gson.toJson(value));
+            byte[] data = bos.toByteArray();
+            if (data.length > 255) {
+                throw new IllegalArgumentException("Too much data sent");
+            }
+            field.setFieldSize((byte) data.length);
+            field.setContent(data);
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+        return field;
+    }
+}
